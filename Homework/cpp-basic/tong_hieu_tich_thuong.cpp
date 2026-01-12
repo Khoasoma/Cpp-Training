12430fbe7fb19d5c2c43bbdfc6d2a428f1ce33ae
khoasoma <songtu438@gmail.com>
2026-01-12 19:39:19 +0700

    Them cac bai tap C++ co ban va nang cao

---
+++ Homework/cpp-basic/tong_hieu_tich_thuong.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+	long double a , b;
+	cin >> a >> b;
+	cout << 1.0*a + b << endl;
+	cout << 1.0*a - b << endl;
+	cout << 1.0*a * b << endl;
+	cout << fixed << setprecision(2) << a / b;
+	return 0;
+}

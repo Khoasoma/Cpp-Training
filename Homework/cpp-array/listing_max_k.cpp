3b75fea855f585e71ffd53c41ae107e7c65d7489
khoasoma <songtu438@gmail.com>
2026-01-13 23:04:30 +0700

    updating homework

---
+++ Homework/cpp-array/listing_max_k.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+using ll = long long;
+
+int main(){
+	int TC; cin >> TC;
+	while(TC--){
+		int n , k; cin >> n >> k;
+		int a[n];
+		for(int i = 0;i < n;i++){
+			
+		}
+	}
+			return 0;
+}

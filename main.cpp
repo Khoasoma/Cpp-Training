00dd8c193cd01c6a333f1b78fde365c865265461
khoasoma <songtu438@gmail.com>
2026-04-04 16:16:01 +0700

    Hoàn thành bài tập: main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    string name; getline(cin, name);
-    cout << "Hello, " << name;
+    int n ,  x; cin >> n >> x;
+    vector<int> a(n);
+    for(int &x : a) cin >> x;
+    if(binary_search(a.begin(),a.end(), x)) cout << "True";
+    else cout << "false";
+    return 0;
 }

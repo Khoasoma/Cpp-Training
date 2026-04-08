28aaf7ba22727f0dc8d2297d3e6cd010fe38775d
khoasoma <songtu438@gmail.com>
2026-04-08 21:32:20 +0700

    upload bài tập: Homework/cpp-prefixsum/sumk.cpp

--- Homework/cpp-prefixsum/sumk.cpp
+++ Homework/cpp-prefixsum/sumk.cpp
@@ -2,20 +2,26 @@
 
 using namespace std;
 
+void solve() {
+    // Your core logic here
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-   	int n ; cin >> n;
-	vector<long long> a(n + 1), pre(n + 1,0);
-	for(int i = 1; i <= n;i++) cin >> a[i];
-
-	for(int i = 1; i <= n;i++) pre[i] = pre[i - 1] + a[i];
-
-	int l , r;
-	cin >> l >> r;
-	
-
+    
+    int n , k; cin >> n >> k;
+    long long curr_sum = 0;long long ans = -1e18;
+    vector<long long> a(n);
+    for(long long &x : a) cin >> x;
+    for(int i = 0; i < n;i++){
+        curr_sum += a[i];
+        if(i >= k)
+            curr_sum -= a[i - k];
+        if(i >= k - 1)
+            ans = max(ans , curr_sum);
+    }
+    cout << ans;
 
     return 0;
 }

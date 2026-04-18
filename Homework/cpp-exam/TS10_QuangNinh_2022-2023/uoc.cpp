982388aaee72823ccc5216d3b5a99094d8005eb3
khoasoma <songtu438@gmail.com>
2026-04-18 21:53:31 +0700

    upload bài tập: Homework/cpp-exam/TS10_QuangNinh_2022-2023/uoc.cpp

---
+++ Homework/cpp-exam/TS10_QuangNinh_2022-2023/uoc.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    // Your core logic here
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+
+    return 0;
+}
